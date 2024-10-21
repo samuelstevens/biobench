f70440328eedea9a58f3974800fe56ea457b1e5e
Samuel Stevens <samuel.robert.stevens@gmail.com>
2024-10-21 14:08:11 -0400

    update beluga task docs

--- biobench/__init__.py
+++ biobench/__init__.py
@@ -10,35 +10,35 @@ The most important modules to understand are:
 * Any of the task modules--`newt` is well documented.
 * Any of the vision modules--`third_party_models.OpenClip` is highly relevant to anyone using the [open_clip](https://github.com/mlfoundations/open_clip) codebase to train models.
 
-## Task Modules
+# Task Modules
 
 * `ages`: Classify images of juvenile birds using only adult birds for training.
 * `beluga`: Re-identification of Beluga whales using nearest neighbors.
 * `birds525`: 1-shot classification with nearest-neighbor of the Kaggle Birds dataset.
 * `fishnet`: Predict presense/absence of 9 different traits in images of fish.
+* `iwildcam`: Species classification from camera trap imagery using multiclass ridge regression.
 * `iwildcam`: Species classification using multiclass linear regression.
 * `kabr`: Behavior classification of single-subject animals using simpleshot of mean frame representations.
 * `newt`: 164 binary classification tasks using an binary SVM trained on image features.
 * `plantnet`:
 * `plankton`: classification of microscopic images of phytoplankton.
 
-## Helper Modules
+# Helper Modules
 
 * `interfaces`: All the interfaces that describe how the different modules communicate.
 * `registry`: How to add/load vision models to BioBench.
 * `simpleshot`: An implementation of nearest-centroid classification from [Simpleshot](https://arxiv.org/abs/1911.04623).
 * `third_party_models`: Some pre-written vision backbones that load checkpoints from [open_clip](https://github.com/mlfoundations/open_clip) and [timm](https://github.com/huggingface/pytorch-image-models). You can use these as inspiration for writing your own vision backbones or check out `biobench.registry` for a tutorial.
 
-## Future Tasks
+# Future Tasks
 
 These are tasks that I plan on adding but are not yet done.
 
 * `rarespecies`: Waiting on a [bug](https://huggingface.co/datasets/imageomics/rare-species/discussions/8) in Huggingface's datasets library.
 * [FishVista](https://github.com/Imageomics/Fish-Vista): I want to add trait classification: given patch-level features, can we train a linear probe to predict the presence/absense of a feature?
 
-.. include:: ./confidence-intervals.md
 
-## Discussion of Task Importance
+# Discussion of Task Importance
 
 (TODO)
 
@@ -50,6 +50,8 @@ Main points:
 * Some are made up and we hypothesize that they predict performance on real tasks (ImageNet-1K, iNat2021).
 * Likely, the real tasks are most important, but they are also the most specialized (and thus least likely to predict performance on other tasks).
 * We don't know if ImageNet-1K or iNat2021 still predict performance on real, specialized tasks. They definitely did 2-5 years ago---is that still true with new models?
+
+.. include:: ./confidence-intervals.md
 """
 
 import typing
