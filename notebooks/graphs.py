b03cedbe164502a9c37f6df1687e94111e434a1f
Samuel Stevens <samuel.robert.stevens@gmail.com>
2025-05-13 14:03:42 -0400

    diffs
    Former-commit-id: 4fb33b30fe7f7ec1c4121f0f157981cb62c98b71

--- notebooks/graphs.py
+++ notebooks/graphs.py
@@ -19,18 +19,86 @@ def _():
 
 
 @app.cell
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 def _(os, pl, sqlite3):
     def load_results():
         df = pl.read_database(
             "SELECT predictions.score, experiments.task_name FROM experiments JOIN predictions WHERE experiments.id = predictions.experiment_id",
             sqlite3.connect(os.path.join("results", "results.sqlite")),
         )
+=======
+=======
+>>>>>>> Stashed changes
+def __(os, sqlite3):
+    conn = sqlite3.connect(os.path.join("reports", "reports.sqlite"))
+    conn.row_factory = sqlite3.Row
+    return (conn,)
+
+
+@app.cell
+def __(np, plt, sqlite3):
+    def plot_task(conn: sqlite3.Connection, task: str) -> plt.Figure | None:
+        fig, ax = plt.subplots()
+        stmt = "SELECT model_ckpt, task_name, mean_score, confidence_lower, confidence_upper, MAX(posix) FROM reports WHERE task_name = (?) GROUP BY model_ckpt, task_name ORDER BY model_ckpt ASC;"
+        data = conn.execute(stmt, (task,)).fetchall()
+
+        if not data:
+            return
+
+        xs = [row["model_ckpt"] for row in data]
+        ys = [row["mean_score"] for row in data]
+
+        yerr = np.array([ys, ys])
+        yerr[0] = np.max(yerr[0] - [row["confidence_lower"] for row in data], 0)
+        yerr[1] = [row["confidence_upper"] for row in data] - yerr[1]
+
+        ax.errorbar(xs, ys, yerr, fmt="o", linewidth=2, capsize=6)
+        ax.set_title(f"Mean {task} Performance")
+        ax.tick_params(axis="x", labelrotation=20)
+
+        fig.tight_layout()
+
+        return fig
+
+    return (plot_task,)
+>>>>>>> Stashed changes
 
         return df
 
+<<<<<<< Updated upstream
     df = load_results()
     df
     return df, load_results
+=======
+@app.cell
+def __(conn, plot_task):
+    plot_task(conn, "KABR")
+    return
+
+
+@app.cell
+def __(conn, plot_task):
+    plot_task(conn, "NeWT")
+    return
+
+
+@app.cell
+def __(conn, plot_task):
+    plot_task(conn, "Pl@ntNet")
+    return
+
+
+@app.cell
+def __(conn, plot_task):
+    plot_task(conn, "iWildCam")
+    return
+
+
+@app.cell
+def __():
+    return
+>>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
